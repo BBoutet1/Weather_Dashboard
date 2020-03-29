546771c3ba99363678505ea50e48bd6906aee383
Brice Boutet <boutetlb@gmail.com>
2020-03-29 01:13:34 -0400

    City not found error handling

--- Develop/script.js
+++ Develop/script.js
@@ -31,6 +31,7 @@ $(document).ready(function(){
         processRequest(city);
     })
 
+
     //Processing acquiring and processing weather data
     function processRequest(city){
         console.log(city);
@@ -46,11 +47,18 @@ $(document).ready(function(){
         // Here we run our AJAX call to the OpenWeatherMap API
         $.ajax({
         url: queryURL,
-        method: "GET"
-        })
+
+            //Successfull ajax call
+            success:ajaxSuccess,
+            
+            //Unsuccessfull ajax call 
+            error: ajaxError,
+        })   
+
         // We store all of the retrieved data inside of an object called "response"
-        .then(function(response) {
+        function ajaxSuccess(response) {
             const location = response.name;
+            city = location // correct city name orthograph (try Lisboa for Lisbon)
             const country = response.sys.country;
             const tempF = Math.round((response.main.temp-273.15)*9/5 + 32); // Kelvin to Fahrenheit
             const tempC = Math.round(response.main.temp-273.15);  // Kelvin to Celcius
@@ -81,63 +89,75 @@ $(document).ready(function(){
                     const uvIndex = response2.value;
                     // Transfer UV index to HTML
                     $(".uvIndex").text("UV index: " + uvIndex);     
-            });
-
-        // Building URL to query current the 5 days forcast database
-        const queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q="+ city+"&appid="+APIKey;
-        $.ajax({
-            url: queryURL3,
-            method: "GET"
-            })
-            // We store UV retrieved data inside of an object called "response2"
-            .then(function(response3) {
-                 // Transfer 5 days forcast in HTML
-                for (let i=0; i<5; i++){
-                    dayI = moment().add(i+1,'days'). format('MM/DD/YYYY')// add 1 day
-                    dayId = "#day"+i;
-                    const j = 8*i+3 // each day have 8 records (every 3hours) - picking up the 4rd one(index 3 at 12:00 PM)
-                    const temp_F = Math.round((response3.list[j].main.temp-273.15)*9/5 + 32); // Kelvin to Fahrenheit
-                    const temp_C = Math.round(response3.list[j].main.temp-273.15); // Kelvin to Celcius
-                    const hum = response3.list[j].main.humidity;
-                    const iconCode3 = response3.list[j].weather[0].icon;
-                    const iconUrl3 = "http://openweathermap.org/img/wn/"+iconCode3+"@2x.png";
-                    $(dayId).empty();
-                    $(dayId).append("<h5>"+dayI+"<\h5>");
-                    $(dayId).append("<img id=\"image\" src=\""+iconUrl3+"\"width=\"50px\" height=\"50px\" alt=\"\">");
-                    $(dayId).append("<p>Temp: " + temp_F+ " °F /"+ temp_C+ " °C<br> Humidity: " +hum+" %</P>");    
-                }
-
              });
 
-        });
+            // Building URL to query current the 5 days forcast database
+            const queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q="+ city+"&appid="+APIKey;
+            $.ajax({
+                url: queryURL3,
+                method: "GET"
+                })
+                // We store UV retrieved data inside of an object called "response2"
+                .then(function(response3) {
+                    // Transfer 5 days forcast in HTML
+                    for (let i=0; i<5; i++){
+                        dayI = moment().add(i+1,'days'). format('MM/DD/YYYY')// add 1 day
+                        dayId = "#day"+i;
+                        const j = 8*i+3 // each day have 8 records (every 3hours) - picking up the 4rd one(index 3 at 12:00 PM)
+                        const temp_F = Math.round((response3.list[j].main.temp-273.15)*9/5 + 32); // Kelvin to Fahrenheit
+                        const temp_C = Math.round(response3.list[j].main.temp-273.15); // Kelvin to Celcius
+                        const hum = response3.list[j].main.humidity;
+                        const iconCode3 = response3.list[j].weather[0].icon;
+                        const iconUrl3 = "http://openweathermap.org/img/wn/"+iconCode3+"@2x.png";
+                        $(dayId).empty();
+                        $(dayId).append("<h5>"+dayI+"<\h5>");
+                        $(dayId).append("<img src=\""+iconUrl3+"\"width=\"50px\" height=\"50px\" alt=\"\">");
+                        $(dayId).append("<p class =\"tempFive\">Temp: " + temp_F+ " °F /"+ temp_C+ " °C<br> Humidity: " +hum+" %</P>");    
+                    }
+
+                });
+
+                       //Check if current city already in the history
+            var index = history.indexOf(city); 
+            if (index >-1){
+                //remove the duplicate if it exists
+                history.splice(index,1);
+            }
+
+            //No more than 10 entries in search history.
+            while( history.length > 9){
+                history.splice(history.length-1,1);
+            }
+
+            // Add the city to search history in the first position
+            history.splice(0,0,city); 
+            
+            //Transfer the city arry in html table
+            $("table").empty();
+            for (let j=0; j<history.length; j++){
+                $("table").append("<tr class=\"history\"><td id=\""+history[j]+"\">"+history[j]+"</td></tr>")
+            }
+
+            //  Clear search History
+            $( ".clearHistory" ).click(function (){
+                $("table").html("<tr><td>No history</td></tr>");
+                history=[];
+             });
+            
+        };
+
+        function ajaxError(){
+            $(".city").text("City of "+city+ " not found");
+            $(".temp").text("Temperature:");
+            $(".humidity").text("Humidity:");
+            $(".wind").text("Wind Speed:");
+            $(".uvIndex").text("UV index:"); 
+            $("img").remove();
+            $(".tempFive").remove();
+        }
+
+ 
 
         
-        //Check if current city already in the history
-        var index = history.indexOf(city); 
-        if (index >-1){
-            //remove the duplicate if it exists
-            history.splice(index,1);
-        }
-
-        //No more than 10 entries in search history.
-        while( history.length > 9){
-            history.splice(history.length-1,1);
-        }
-
-
-        // Add the city to search history in the first position
-        history.splice(0,0,city); 
-        
-        //Transfer the city arry in html table
-        $("table").empty();
-        for (let j=0; j<history.length; j++){
-            $("table").append("<tr class=\"history\"><td id=\""+history[j]+"\">"+history[j]+"</td></tr>")
-        }
-
-        //  Clear search History
-        $( ".clearHistory" ).click(function (){
-            $("table").html("<tr><td>No history</td></tr>");
-            history=[]
-        });
     }
 });
